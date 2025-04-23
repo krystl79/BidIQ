2b83095bbd259ceb896fc2f6b5be3c3097f96a90
Krystal Philipps <krystl79@gmail.com>
2025-04-22 21:55:13 -0700

    chore: update ESLint configuration to ignore service worker errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,5 +3,14 @@ module.exports = {
   rules: {
     'no-unused-vars': 'warn',
     'react-hooks/exhaustive-deps': 'warn'
-  }
+  },
+  overrides: [
+    {
+      files: ['src/service-worker.js'],
+      rules: {
+        'no-restricted-globals': 'off',
+        'no-undef': 'off'
+      }
+    }
+  ]
 }; 
